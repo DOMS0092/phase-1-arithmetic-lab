8e079b8eed9344fbd6adb9edb1f4882531849e10
kendra Hood <my.github.email@example.com>
2021-10-10 16:53:26 -0500

    commit tree bf4b31e93e43cc803f8b507682e14cac8263a569

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31, num2 = 2;
+let multiply = num1 * num2;
+
+
+let random = 0;
+random += 1;
+
+const num3 = 19, num4 = 5;
+let mod = num3 % num4;
+
+let max = (15, 16, 17, 18, 19, 20);
+Math.max(15, 16, 17, 18, 19, 20);
